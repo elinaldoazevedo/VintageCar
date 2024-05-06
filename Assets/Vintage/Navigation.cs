8246901b3fa71a94d8b36eb2660c856928361a05
ELINALDO RIBEIRO DE AZEVEDO <elinaldoazevedo@gmail.com>
2024-05-06 14:37:23 -0300

    Pastas do projeto
    Pastas e arquivos do projeto.

---
+++ Assets/Vintage/Navigation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour {
+
+    public Transform chegada;
+    private UnityEngine.AI.NavMeshAgent auxPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        auxPos = transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        auxPos.destination = chegada.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //auxPos.destination = ene2.position;
+    }
+}
