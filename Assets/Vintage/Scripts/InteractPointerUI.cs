6aa8070d6ac259fdae046208418df1fbfab0656c
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-05-09 17:32:54 -0300

    feat: text mesh pro ugui added.

---
+++ Assets/Vintage/Scripts/InteractPointerUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractPointerUI : MonoBehaviour
+{
+    [SerializeField] Image _pointerFill = null;
+    [SerializeField] TextMeshProUGUI _nameText = null;
+}
