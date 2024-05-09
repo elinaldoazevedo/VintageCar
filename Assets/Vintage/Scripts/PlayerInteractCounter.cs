dc2acde2ebc4de8557680afb55f81b0154d86704
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-05-09 17:25:00 -0300

    feat: player interact counter added.

---
+++ Assets/Vintage/Scripts/PlayerInteractCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractCounter : MonoBehaviour
+{
+    [SerializeField] PlayerInteractPointer _playerInteractPointer = null;
+    [SerializeField] float _startTime = 1f;
+    [SerializeField] float _interactTime = 3f;
+    [Space]
+    [SerializeField] InteractableElement _interactableElement = null;
+    [SerializeField] float _timer = 0f;
+
+    private void OnEnable()
+    {
+        _playerInteractPointer.onInteractableChange += ResetTimer;
+    }
+
+    private void OnDisable()
+    {
+        _playerInteractPointer.onInteractableChange -= ResetTimer;
+    }
+
+    private void Update()
+    {
+        if (_interactableElement == null) return;
+
+        _timer += Time.deltaTime;
+
+        if (_timer <= _startTime) return;
+
+        // onInteractionTimerStarted.
+        
+        if (_timer >= _interactTime)
+        {
+            // onInteractionTimerEnd.
+        }
+    }
+
+    private void ResetTimer(InteractableElement _element)
+    {
+        _interactableElement = _element;
+        _timer = 0;
+    }
+}
