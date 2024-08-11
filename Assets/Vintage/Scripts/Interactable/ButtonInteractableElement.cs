595a0dc247494ca57ad64b213192feaf6316a4ec
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-08-11 15:56:04 -0300

    feat: quit game button added.

---
+++ Assets/Vintage/Scripts/Interactable/ButtonInteractableElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteractableElement : InteractableElement
+{
+    [SerializeField] Button _button = null;
+
+    private void OnValidate()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    public override void Interact()
+    {
+        if (_button.onClick != null)
+            _button.onClick.Invoke();
+
+        base.Interact();
+    }
+}
