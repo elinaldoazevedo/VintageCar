a7329824de474aeb9a5b8051de37522eea69aa02
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-08-11 16:35:12 -0300

    feat: load scene from main menu to game added.

---
+++ Assets/Vintage/Scripts/LoadSceneHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneHandler : MonoBehaviour
+{
+    [SerializeField] string _loadScene = null;
+    //[SerializeField] string _unloadScene = null;
+
+    public static event System.Action OnStartLoad = null;
+    public static event System.Action OnEndLoad = null;
+
+    public void LoadScene()
+    {
+        //Debug.Log("// Start Load");
+        //SceneManager.LoadSceneAsync(_loadScene);
+        //Debug.Log("// End Load");
+        StartCoroutine(Load_routine());
+    }
+
+    private IEnumerator Load_routine()
+    {
+        if (OnStartLoad != null)
+            OnStartLoad.Invoke();
+
+        //if (!string.IsNullOrEmpty(_unloadScene))
+        //{
+        //    var _asyncUnload = SceneManager.UnloadSceneAsync(_unloadScene);
+
+        //    while (!_asyncUnload.isDone)
+        //    {
+        //        yield return null;
+        //    }
+
+        //    Debug.Log("// Scene Unloaded");
+        //}
+
+        var _asyncLoad = SceneManager.LoadSceneAsync(_loadScene/*, LoadSceneMode.Additive*/);
+
+        while (!_asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        if (OnEndLoad != null)
+            OnEndLoad.Invoke();
+    }
+}
---
+++ Assets/Vintage/Scripts/UI/LoadView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadView : MonoBehaviour
+{
+    [SerializeField] CanvasGroup _canvasGroup = null;
+
+    private void OnValidate()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void OnEnable()
+    {
+        LoadSceneHandler.OnStartLoad += LoadSceneHandler_OnStartLoad;
+        LoadSceneHandler.OnEndLoad += LoadSceneHandler_OnEndLoad;
+    }
+
+    private void OnDisable()
+    {
+        LoadSceneHandler.OnStartLoad -= LoadSceneHandler_OnStartLoad;
+        LoadSceneHandler.OnEndLoad -= LoadSceneHandler_OnEndLoad;
+    }
+
+    private void LoadSceneHandler_OnStartLoad()
+    {
+        _canvasGroup.alpha = 1;
+    }
+
+    private void LoadSceneHandler_OnEndLoad()
+    {
+        _canvasGroup.alpha = 0;
+    }
+}
