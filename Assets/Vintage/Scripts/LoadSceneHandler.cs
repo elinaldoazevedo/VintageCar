f6f094bd3f1ac59e828170a765b585f099461f9c
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-08-12 10:51:59 -0300

    minor scene adjustments.

--- Assets/Vintage/Scripts/LoadSceneHandler.cs
+++ Assets/Vintage/Scripts/LoadSceneHandler.cs
@@ -5,9 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneHandler : MonoBehaviour
 {
-    [SerializeField] string _loadScene = null;
-    //[SerializeField] string _unloadScene = null;
-
     public static event System.Action OnStartLoad = null;
     public static event System.Action OnEndLoad = null;
 
@@ -42,42 +39,6 @@ public class LoadSceneHandler : MonoBehaviour
         }
 
         var _asyncLoad = SceneManager.LoadSceneAsync(_loadScene, LoadSceneMode.Additive);
-
-        while (!_asyncLoad.isDone)
-        {
-            yield return null;
-        }
-
-        if (OnEndLoad != null)
-            OnEndLoad.Invoke();
-    }
-
-    public void LoadScene()
-    {
-        //Debug.Log("// Start Load");
-        //SceneManager.LoadSceneAsync(_loadScene);
-        //Debug.Log("// End Load");
-        StartCoroutine(Load_routine());
-    }
-
-    private IEnumerator Load_routine()
-    {
-        if (OnStartLoad != null)
-            OnStartLoad.Invoke();
-
-        //if (!string.IsNullOrEmpty(_unloadScene))
-        //{
-        //    var _asyncUnload = SceneManager.UnloadSceneAsync(_unloadScene);
-
-        //    while (!_asyncUnload.isDone)
-        //    {
-        //        yield return null;
-        //    }
-
-        //    Debug.Log("// Scene Unloaded");
-        //}
-
-        var _asyncLoad = SceneManager.LoadSceneAsync(_loadScene/*, LoadSceneMode.Additive*/);
 
         while (!_asyncLoad.isDone)
         {
