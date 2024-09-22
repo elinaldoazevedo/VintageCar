8f3d7a45ed069df00106a54f879ee8127cfd1aaf
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-09-22 18:56:48 -0300

    agahghalghlakglkaglk

--- Assets/Vintage/Scripts/PlayerInteractPointer.cs
+++ Assets/Vintage/Scripts/PlayerInteractPointer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerInteractPointer : MonoBehaviour
 {
     [SerializeField] Transform _origin = null;
+    //[SerializeField] Transform _pointer = null;
+    //[SerializeField] GvrReticlePointer _gvrReticle = null;
+    //[SerializeField] float _maxPointerDistance = 0.1f;
     [SerializeField] LayerMask _layerMask = 0;
     [SerializeField] float _maxDistance = 20f;
     [SerializeField] float _radius = 1f;
@@ -15,7 +18,7 @@ public class PlayerInteractPointer : MonoBehaviour
 
     public static event System.Action<InteractableElement> OnInteractableChange = null;
 
-    private void Update()
+    private void LateUpdate()
     {
         var _lastInteractable = _interactableElement;
         _interactableElement = SearchForInteractable();
@@ -35,9 +38,25 @@ public class PlayerInteractPointer : MonoBehaviour
         for (int i = 0; i < _hits; i++)
         {
             var _collider = _results[i].collider;
+            //Fodase();
+
             return _collider.GetComponent<InteractableElement>();
         }
 
+        //_pointer.position = _origin.position + _origin.forward * _maxPointerDistance;
+        //Fodase();
+
         return null;
     }
+
+    //public void Fodase()
+    //{
+    //    //var _pointHit = _results[i].point;
+    //    var _pointHit = _gvrReticle._pos;
+    //    var difference = _pointHit - _origin.position;
+    //    var direction = difference.normalized;
+    //    var distance = Mathf.Min(_maxPointerDistance, difference.magnitude);
+    //    var endPosition = _origin.position + direction * distance;
+    //    _pointer.position = endPosition;
+    //}
 }
