7ae562c142c3c374fc4920600fda4dd0c68a9387
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-09-22 20:11:39 -0300

    more ui size adjustments.

--- Assets/Vintage/Scripts/FollowCamera.cs
+++ Assets/Vintage/Scripts/FollowCamera.cs
@@ -8,68 +8,10 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] Camera _camera = null;
     [SerializeField] float _forwardMultiplier = 1f;
 
-    //[SerializeField] LayerMask _layerMask = 0;
-    //[SerializeField] float _minDistance = 0.5f;
-    //[SerializeField] float _maxDistance = 1f;
-    //[SerializeField] float _radius = 0.1f;
-    //[SerializeField] QueryTriggerInteraction _queryTriggerInteraction;
-
-    //public Vector3 _point;
-    //private RaycastHit[] _results = new RaycastHit[1];
-
     private void LateUpdate()
     {
         var _cameraTransform = _camera.transform;
         _transform.position = _cameraTransform.position + _cameraTransform.forward * _forwardMultiplier;
         _transform.rotation = _cameraTransform.rotation;
-
-        //SearchForInteractable();
-        //return;
-        //var _pointHit = SearchForInteractable();
-        //var difference = _pointHit - _camera.transform.position;
-        //var direction = difference.normalized;
-        //var distance = Mathf.Min(0.5f, difference.magnitude);
-        //var endPosition = _camera.transform.position + direction * distance;
-        //_transform.position = endPosition;
-        //_transform.rotation = _camera.transform.rotation;
     }
-
-    //private Vector3 SearchForInteractable()
-    //{
-    //    var _ray = new Ray(_camera.transform.position, _camera.transform.forward);
-    //    int _hits = Physics.RaycastNonAlloc(_ray, _results, _maxDistance, _layerMask, _queryTriggerInteraction);
-    //    //int _hits = Physics.SphereCastNonAlloc(_ray, _radius, _results, _maxDistance, _layerMask, _queryTriggerInteraction);
-
-    //    for (int i = 0; i < _hits; i++)
-    //    {
-    //        var _collider = _results[i].collider;
-
-    //        var _pointHit = _results[i].point;
-    //        var difference = _pointHit - _camera.transform.position;
-    //        var direction = difference.normalized;
-    //        var distance = Mathf.Min(_minDistance, difference.magnitude);
-    //        var endPosition = _camera.transform.position + direction * distance;
-
-    //        _transform.position = _pointHit + -_camera.transform.forward * 0.01f;
-    //        _transform.rotation = _camera.transform.rotation;
-    //        _point = _pointHit;
-    //        Debug.Log(_collider.name);
-    //        return Vector3.zero;
-    //    }
-
-    //    _transform.position = _camera.transform.position + _camera.transform.forward * _minDistance;
-    //    _transform.rotation = _camera.transform.rotation;
-    //    return Vector3.zero;
-    //    //return _camera.transform.position + _camera.transform.forward * _maxDistance;
-    //}
-
-    //public void Fodase()
-    //{
-    //    var _pointHit = _results[i].point;
-    //    var difference = _pointHit - _camera.transform.position;
-    //    var direction = difference.normalized;
-    //    var distance = Mathf.Min(0.5f, difference.magnitude);
-    //    var endPosition = _camera.transform.position + direction * distance;
-    //    _transform.position = endPosition;
-    //}
 }
