7b2ca0d754bee7df023a13b58a3392f25047fd9d
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-09-23 18:01:55 -0300

    clamp z axis added to the follow camera script.

--- Assets/Vintage/Scripts/FollowCamera.cs
+++ Assets/Vintage/Scripts/FollowCamera.cs
@@ -7,11 +7,20 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] Transform _transform = null;
     [SerializeField] Camera _camera = null;
     [SerializeField] float _forwardMultiplier = 1f;
+    [SerializeField] bool _clampZ = false;
 
     private void LateUpdate()
     {
         var _cameraTransform = _camera.transform;
         _transform.position = _cameraTransform.position + _cameraTransform.forward * _forwardMultiplier;
-        _transform.rotation = _cameraTransform.rotation;
+        _transform.rotation = _clampZ ? GetRotation() : _cameraTransform.rotation;
+        //_transform.rotation = _cameraTransform.rotation;
+    }
+
+    private Quaternion GetRotation()
+    {
+        var _cameraEuler = _camera.transform.rotation.eulerAngles;
+        _cameraEuler.z = 0;
+        return Quaternion.Euler(_cameraEuler);
     }
 }
