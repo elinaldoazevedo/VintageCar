6d277357b0fb96a58a807fb6566e88391d7ce5ba
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-10-16 15:23:37 -0300

    fix: restart button not working.

--- Assets/Vintage/Scripts/UI/EnterButtonHelper.cs
+++ Assets/Vintage/Scripts/UI/EnterButtonHelper.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnterButtonHelper : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent _agent = null;
+    [SerializeField] float _moveSpeed = 2f;
+
+    public void Init()
+    {
+        _agent.speed = _moveSpeed;
+    }
 }
